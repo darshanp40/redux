8e360f385aedba95615477b198811eaba679371a
darshanp <darshanp@zeta.tech>
2019-09-04 16:41:43 +0530

    react-redux

---
+++ ConnectProvicerAPI.js
@@ -0,0 +1,8 @@
+/* 
+    knows when the store changes - use store.subscribe
+    what state is needed - use mapStateToProps
+    get the store from the context
+*/
+function connect(mapStateToProps) {
+    return Component;
+}
